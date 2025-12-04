8d4e9a3052e8a0fdb2da762bb0a42f83d232969c
Jelle Meeus <sogladev@gmail.com>
2025-12-04 18:09:41 +0100

    fixup! fixup! fix: remove Strat config requirement as default, update entrance helpers

--- src/Naxxramas/scripts/naxxramas_40.cpp
+++ src/Naxxramas/scripts/naxxramas_40.cpp
@@ -1,18 +1,19 @@
 #include "Player.h"
+#include "VanillaNaxxramas.h"
 #include "naxxramas_40.h"
 
-bool IsAttuned(Player const& player)
+bool IsAttuned(Player const* player)
 {
-    if ((player.GetQuestStatus(NAXX40_ATTUNEMENT_1) == QUEST_STATUS_REWARDED) ||
-        (player.GetQuestStatus(NAXX40_ATTUNEMENT_2) == QUEST_STATUS_REWARDED) ||
-        (player.GetQuestStatus(NAXX40_ATTUNEMENT_3) == QUEST_STATUS_REWARDED))
+    if ((player->GetQuestStatus(NAXX40_ATTUNEMENT_1) == QUEST_STATUS_REWARDED) ||
+        (player->GetQuestStatus(NAXX40_ATTUNEMENT_2) == QUEST_STATUS_REWARDED) ||
+        (player->GetQuestStatus(NAXX40_ATTUNEMENT_3) == QUEST_STATUS_REWARDED))
     {
         return true;
     }
     return false;
 }
 
-bool CanEnterNaxx40(Player const& player)
+bool CanEnterNaxx40(Player const* player)
 {
     return IsAttuned(player) || !sVanillaNaxxramas->requireAttunement;
 }
